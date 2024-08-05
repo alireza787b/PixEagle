b8cd4edc12c800c8a14fc713f2a0ccc9aa55b0a9
Alireza Ghaderi <p30planets@gmail.com>
2024-08-05 23:34:49 +0000

    Create useStatuses.js

---
+++ dashboard/src/hooks/useStatuses.js
@@ -0,0 +1,59 @@
+//dashboard/src/hooks/useStatuses.js
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const API_URL = `http://${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_API_PORT}`;
+
+export const useTrackerStatus = (interval = 2000) => {
+  const [isTracking, setIsTracking] = useState(false);
+
+  useEffect(() => {
+    const fetchTrackerStatus = async () => {
+      try {
+        const response = await axios.get(`${API_URL}/telemetry/tracker_data`);
+        const trackerData = response.data;
+
+        if (trackerData.tracker_started) {
+          setIsTracking(true);
+        } else {
+          setIsTracking(false);
+        }
+      } catch (error) {
+        console.error('Error fetching tracker data:', error);
+        setIsTracking(false);
+      }
+    };
+
+    const intervalId = setInterval(fetchTrackerStatus, interval);
+    fetchTrackerStatus(); // Initial call
+
+    return () => clearInterval(intervalId);
+  }, [interval]);
+
+  return isTracking;
+};
+
+export const useFollowerStatus = (interval = 2000) => {
+  const [isFollowing, setIsFollowing] = useState(false);
+
+  useEffect(() => {
+    const fetchFollowerStatus = async () => {
+      try {
+        const response = await axios.get(`${API_URL}/telemetry/follower_data`);
+        const followerData = response.data;
+
+        setIsFollowing(followerData.following_active);
+      } catch (error) {
+        console.error('Error fetching follower data:', error);
+        setIsFollowing(false);
+      }
+    };
+
+    const intervalId = setInterval(fetchFollowerStatus, interval);
+    fetchFollowerStatus(); // Initial call
+
+    return () => clearInterval(intervalId);
+  }, [interval]);
+
+  return isFollowing;
+};
